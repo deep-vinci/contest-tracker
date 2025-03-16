1a224032a729a0b7ca10808382ff22f45b69dad8
Deepak <deepakworkpc@gmail.com>
2025-03-16 15:40:23 +0530

    feat: add the user check for db

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,10 +1,32 @@
 const express = require("express");
 const path = require("path");
+const { createClient } = require("@supabase/supabase-js");
+
+require("dotenv").config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 // const jwtDecode = require("jwt-decode");
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/api/userinfo", async (req, res) => {
+    // check if user exists
+    const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("email", req.body.data.email);
+
+    if (error) throw error;
+    console.log(data.length > 0 ? data[0] : null);
+
+    res.json({ message: "Hello from API!" });
+});
 
 app.get("/g_auth", (req, res) => {
     console.log(req.body);
