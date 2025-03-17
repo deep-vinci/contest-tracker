b5f45f99fe9c183e14f41ed452501effa6d2d397
Deepak <deepakworkpc@gmail.com>
2025-03-18 01:38:13 +0530

    chore: update the route to reflect proper nameing

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,36 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 function Login({ updateUser }) {
-    const veirfyJWT = async (data) => {
+    const postUserData = async (data, jwt) => {
+        try {
+            const response = await axios.post(
+                "/api/auth/signup-or-login",
+                {
+                    data,
+                    jwt,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            console.log("User data posted successfully:", response.data);
+            return response.data; // Return the response if needed
+        } catch (error) {
+            console.error("Error posting user data:", error);
+            throw error; // Handle error properly
+        }
+    };
+
+    const updateUserState = (data) => {
+        const jwt = data;
         const decodedData = jwtDecode(data.credential);
         updateUser(decodedData);
-        return decodedData;
+        postUserData(decodedData, jwt);
     };
 
     const errorMessage = (error) => {
@@ -13,16 +38,14 @@ function Login({ updateUser }) {
     };
     return (
         <>
-            <div className="w-fit">
-                <GoogleLogin
-                    theme="outline"
-                    type="standard"
-                    size="medium"
-                    useOneTap="true"
-                    onSuccess={veirfyJWT}
-                    onError={errorMessage}
-                />
-            </div>
+            <GoogleLogin
+                theme="outline"
+                type="standard"
+                size="medium"
+                useOneTap="true"
+                onSuccess={updateUserState}
+                onError={errorMessage}
+            />
         </>
     );
 }
