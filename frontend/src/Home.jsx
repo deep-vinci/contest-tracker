eb3f1b21ef66cb776d35079512cc96b080224af0
Deepak <deepakworkpc@gmail.com>
2025-03-18 18:41:51 +0530

    chore: clear up the code, implementing the useContext

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
-import axios from "axios";
 
 function Home() {
-    const [loggedIn, setLoggedIn] = useState("true");
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const autoLogin = async () => {
-            try {
-                const response = await axios.get("/api/auth/session", {
-                    withCredentials: true,
-                });
-                console.log("test", response.data.user[0]);
-                setUser(response.data.user[0]);
-                setLoggedIn("true");
-            } catch (error) {
-                console.error("err", error);
-            }
-        };
-
-        autoLogin();
-    }, []);
-
     return (
         <>
-            <Navigation user={user} updateUser={setUser} />
+            <Navigation />
         </>
     );
 }
