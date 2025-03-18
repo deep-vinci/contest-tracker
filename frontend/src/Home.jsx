53b6a4a0309ab75cac110ba0974ef530e4da456b
Deepak <deepakworkpc@gmail.com>
2025-03-19 01:22:39 +0530

    feat: use loading state to dispaly skeleton during loading and change the ui to hero or dashboard after loading ends depending on user object

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,26 @@
 import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Dashboard from "./components/Dashboard";
+import { useUser } from "./context/UserContext";
+import SkeletonTable from "./components/SkeletonTable";
 
 function Home() {
+    const { user, setUser, loading } = useUser();
+
+    console.log(loading);
+    // console.log("user", user);
     return (
-        <>
+        <div className="w-full max-w-[1400px] mx-auto">
             <Navigation />
-        </>
+            {loading ? (
+                <SkeletonTable />
+            ) : user === null ? (
+                <Hero />
+            ) : (
+                <Dashboard />
+            )}
+            {/* <Dashboard /> */}
+        </div>
     );
 }
 
