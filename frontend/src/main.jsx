1b6a2bba6a80fa0e4c1160550f6e19eff0f5ca81
Deepak <deepakworkpc@gmail.com>
2025-03-18 18:49:22 +0530

    feat: implement UserProvider context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { UserProvider } from "./context/UserContext.jsx";
 
 import "./index.css";
 import Home from "./Home.jsx";
 import About from "./About.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
-    </Router>
+    <GoogleOAuthProvider clientId="653138620265-atc15v2ejb98cbaioavvqef9j0gm3sud.apps.googleusercontent.com">
+        <UserProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </Router>
+        </UserProvider>
+    </GoogleOAuthProvider>
 );
