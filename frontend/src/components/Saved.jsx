478c2466214d7c2356517aa5dffa7aac8a4e8729
Deepak <deepakworkpc@gmail.com>
2025-03-27 11:13:39 +0530

    featt: add the save comp

---
+++ frontend/src/components/Saved.jsx
@@ -0,0 +1,9 @@
+function Saved() {
+    return (
+        <div className="w-full h-[70vh] flex justify-center items-center">
+            <div>boring work, won't learn anything new implementing this</div>
+        </div>
+    );
+}
+
+export default Saved;
